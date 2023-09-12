4e490b317050721ba84053683803aad6d2379ea8
deepakganesh1234 <144809917+deepakganesh1234@users.noreply.github.com>
2023-09-12 23:01:25 +0530

    Update comparingweightofprotein.py

--- comparingweightofprotein.py
+++ comparingweightofprotein.py
@@ -1,11 +1,1 @@
-Exercise1
-prot2 = float( input('what is the weight of protein 2: '))
-
-prot4 = float( input('what is the weight of protein 4: '))
-
-print(prot2 > prot4)
-
-print(prot2 < prot4)
-
-print(prot2 < prot4)
 
